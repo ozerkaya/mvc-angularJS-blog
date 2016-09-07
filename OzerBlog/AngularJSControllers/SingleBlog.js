e8e4f53e9114f64d516f196418eb9b895c719b34
ozer.kaya <ozer.kaya@bimsa.com.tr>
2016-09-08 01:27:56 +0300

    captcha for comments

--- OzerBlog/AngularJSControllers/SingleBlog.js
+++ OzerBlog/AngularJSControllers/SingleBlog.js
@@ -26,3 +26,61 @@ myApp.controller("SingleBlogController", function ($scope, $http) {
     };
 
 });
+
+myApp.directive('simpleCaptcha', function () {
+    return {
+        restrict: 'E',
+        scope: { valid: '=' },
+        template: '<input ng-model="a.value" ng-show="a.input" style="width:2em; text-align: center;"><span ng-hide="a.input">{{a.value}}</span>&nbsp;{{operation}}&nbsp;<input ng-model="b.value" ng-show="b.input" style="width:2em; text-align: center;"><span ng-hide="b.input">{{b.value}}</span>&nbsp;=&nbsp;{{result}}',
+        controller: function ($scope) {
+
+            var show = Math.random() > 0.5;
+
+            var value = function (max) {
+                return Math.floor(max * Math.random());
+            };
+
+            var int = function (str) {
+                return parseInt(str, 10);
+            };
+
+            $scope.a = {
+                value: show ? undefined : 1 + value(4),
+                input: show
+            };
+            $scope.b = {
+                value: !show ? undefined : 1 + value(4),
+                input: !show
+            };
+            $scope.operation = '+';
+
+            $scope.result = 5 + value(5);
+
+            var a = $scope.a;
+            var b = $scope.b;
+            var result = $scope.result;
+
+            var checkValidity = function () {
+                if (a.value && b.value) {
+                    var calc = int(a.value) + int(b.value);
+                    $scope.valid = calc == result;
+                } else {
+                    $scope.valid = false;
+                }
+                $scope.$apply(); // needed to solve 2 cycle delay problem;
+            };
+
+
+            $scope.$watch('a.value', function () {
+                checkValidity();
+            });
+
+            $scope.$watch('b.value', function () {
+                checkValidity();
+            });
+
+
+
+        }
+    };
+});
